7dd30739c58efca25ba5d786adde03839a619177
Shalitha Suranga <shalithas@99x.lk>
2019-02-21 16:07:22 +0530

    Adding files :rocket:

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld type="vue"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/HelloWorld';
 
 @Component({
   components: {
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vueCustomElement from 'vue-custom-element'
+import HelloWorld from '@/components/HelloWorld';
+
+
+Vue.use(vueCustomElement);
 
 Vue.config.productionTip = false
+
+
+Vue.customElement("hello-world", new HelloWorld().$options);
 
 new Vue({
   render: h => h(App),
